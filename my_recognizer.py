843b1dc9b13e4d797bd260bb485069714bfab2cc
Reza <dr.mrdodge@gmail.com>
2018-04-11 19:54:23 -0700

    FEAT: Add the code for my_recognizer.py

--- my_recognizer.py
+++ my_recognizer.py
@@ -18,8 +18,24 @@ def recognize(models: dict, test_set: SinglesData):
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
    """
     warnings.filterwarnings("ignore", category=DeprecationWarning)
-    probabilities = []
-    guesses = []
+    scores = [{} for _ in range(test_set.num_items)]
+    guesses = ["" for _ in range(test_set.num_items)]
+    for i in range(test_set.num_items):
+        X, lengths = test_set.get_item_Xlengths(i)
+        best_score = float("-inf")
+        best_word = ""
+        for word, model in models.items():
+            try:
+                score = model.score(X, lengths)
+                scores[i][word] = score
+                if score > best_score:
+                    best_score = score
+                    best_word = word
+            except:
+                pass
+        guesses[i] = best_word
+
     # TODO implement the recognizer
     # return probabilities, guesses
-    raise NotImplementedError
+    return scores, guesses
+
